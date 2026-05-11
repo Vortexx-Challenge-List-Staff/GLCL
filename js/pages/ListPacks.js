3538b4f83f8d81462fab9dacc40e8c1148d374f7
Shialaxy <maximv.2011.v@gmail.com>
2026-05-11 18:16:43 +0200

    .

--- js/pages/ListPacks.js
+++ js/pages/ListPacks.js
@@ -27,5 +27,16 @@ export default {
                 </div>
             </div>
         </div>
-    `,
+    <div class="video-window">
+    <iframe 
+        width="100%" 
+        height="100%" 
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowfullscreen>
+    </iframe>
+</div>
+        `,
 };
