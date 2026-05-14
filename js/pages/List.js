0cf4f09acb07ac8c8a99eb1f437dd8e122c24463
Shialaxy <maximv.2011.v@gmail.com>
2026-05-14 12:22:28 +0200

    .

--- js/pages/List.js
+++ js/pages/List.js
@@ -39,8 +39,9 @@ export default {
             <div class="level-container">
                 <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
-                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
-                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
+<p v-if="level.description" style="margin-bottom: 15px;">{{ level.description }}</p>
+<LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
+<iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
