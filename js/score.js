6a0d9ee0e54ade32639abace1263d10b179632b2
Shialaxy <maximv.2011.v@gmail.com>
2026-05-30 21:53:17 +0200

    .

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 48) {
         return 0;
     }
-    if (rank > 75 && percent < 100) {
+    if (rank > 48 && percent < 100) {
         return 0;
     }
 
